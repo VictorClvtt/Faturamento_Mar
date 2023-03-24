c25d71c552fe3cdad851a1b15aa43102b391b09a
Victor Clivatti <joaoclivatti@hotmail.com>
2023-03-24 14:32:20 -0300

    Correção JSON

--- Faturamento_Mar.py
+++ Faturamento_Mar.py
@@ -1,23 +1,32 @@
 import json
 
-with open('faturamento_mar.json', 'r') as file:
-    fat_dia = json.load(file)
-
+# Carregar o arquivo JSON com os dados do faturamento
+with open('dados.json', 'r') as file:
+	fat_dia = json.load(file)
+	
+# Exibição dos dias e faturamentos
 print("Faturamento do mês de Março:\n")
-for k, v in fat_dia.items():
-    print(k, ":", v)
 
-fat_dia_ordenado = sorted(fat_dia.items(), key=lambda x: x[1])
+for item in fat_dia:
+	print(item['dia'], ":", item['valor'])
+
+# Cálculo e exibição dos valores ordenados pelo faturamento
+fat_dia_ordenado = sorted(fat_dia, key=lambda x: x['valor'])
 
 print("\nFaturamento do mês de Março ordenado:\n")
-for k, v in fat_dia_ordenado:
-    print(k, ":", v)
-    
-media=sum(int(v) for v in fat_dia.values()) / len(fat_dia)
+
+for item in fat_dia_ordenado:
+	print(item['dia'], ":", item['valor'])
+
+# Cálculo e exibição da média de faturamento
+valores = [item['valor'] for item in fat_dia]
+media = sum(valores) / len(valores)
+
 print("\nA média de faturamento do mês de Março foi de:", round(media, 2))
 
-maxi=max(fat_dia, key=lambda k: fat_dia[k])
-mini=min(fat_dia, key=lambda k: fat_dia[k])
+# Cálculo e exibição do menor e do maior faturamento
+max_item = max(fat_dia, key=lambda item: item['valor'])
+min_item = min(fat_dia, key=lambda item: item['valor'])
 
-print("\nMaior faturamento(", maxi, ":", fat_dia[maxi], ")")
-print("Menor faturamento(", mini, ":", fat_dia[mini], ")")
+print("\nMaior faturamento(", max_item['dia'], ":", max_item['valor'], ")")
+print("Menor faturamento(", min_item['dia'], ":", min_item['valor'], ")")
